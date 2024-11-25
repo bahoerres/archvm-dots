c899f84d04faafc4bb3957751cbcef63bea5fb39
bahoerres <61599465+bahoerres@users.noreply.github.com>
2024-11-25 07:08:51 -0600

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -19,6 +19,3 @@ fish_add_path /usr/lib/ccache/bin
 # Initialize Starship prompt
 starship init fish | source
 
-set -gx ANTHROPIC_API_KEY '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'
-
-set -gx OPENAI_API_KEY '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'
